0198d064afaac59d9e27427e3b27d4863a5834b5
ajstover07 <amandajstover@gmail.com>
2022-09-02 14:37:07 -0400

    commit tree ea2e67bcb4fd5f2fa03e2d8335637a45e878b3c1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+    //const result = drivers.filter(drivers => drivers.length > 6);
+    function findMatching(source, driver) {
+        return source.filter(
+            (possibleMatch) => possibleMatch.toLowerCase() === driver.toLowerCase()
+        ) 
+    } 
+
+    function fuzzyMatch(source, string) {
+        return source.filter((possibleMatch) => possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0)
+    }
+    
+    function matchName(source, driverName){
+        return source.filter((record) => record.name === driverName)
+    }
+  
+ 
+    
+
+
+    
+//should take an array of drivers' names and a string as an argument
+//return the matching list of drivers
+//function should be case insensitive
+
+
+
